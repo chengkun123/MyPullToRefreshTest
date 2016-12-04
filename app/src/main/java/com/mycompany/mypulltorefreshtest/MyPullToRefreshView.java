56139604200e0b9ae6cd37bda03d8eb26dbba18d
chengkun_123 <bsbs474747@163.com>
2016-12-04 20:08:39 +0800

    readme

--- app/src/main/java/com/mycompany/mypulltorefreshtest/MyPullToRefreshView.java
+++ app/src/main/java/com/mycompany/mypulltorefreshtest/MyPullToRefreshView.java
@@ -258,9 +258,9 @@ public class MyPullToRefreshView extends LinearLayout implements View.OnTouchLis
         }
     }
     public interface PullToRefreshListener {
-
         /**
-         * 刷新时会去回调此方法，在方法内编写具体的刷新逻辑。注意此方法是在子线程中调用的， 你可以不必另开线程来进行耗时操作。
+         * 这个回调在主线程中进行，所以要另开线程进行耗时操作
+         *
          */
         void onRefresh();
 
